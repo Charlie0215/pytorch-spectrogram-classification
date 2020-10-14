a72fe9edf98986fdb487db36dc64eb00b9b1d97f
Charlie0215 <dlf772@gmail.com>
2020-10-14 13:32:33 -0400

    update

--- models/squeezenet_multi.py
+++ models/squeezenet_multi.py
@@ -7,6 +7,18 @@ model_urls = {
     'squeezenet1_1': 'https://download.pytorch.org/models/squeezenet1_1-f364aa15.pth',
 }
 
+class MakeDense(nn.Module):
+    def __init__(self, in_channels, growth_rate, kernel_size=3):
+        super(MakeDense, self).__init__()
+        self.conv = nn.Conv2d(in_channels, growth_rate, kernel_size=kernel_size, padding=(kernel_size-1)//2)
+        self.norm_layer = nn.BatchNorm2d(growth_rate)
+
+    def forward(self, x):
+        out = F.relu(self.conv(x))
+        out = self.norm_layer(out)
+        out = torch.cat((x, out), 1)
+        return out
+
 class Fire(nn.Module):
     def __init__(self, inplanes, squeeze_planes,
                  expand1x1_planes, expand3x3_planes):
@@ -31,10 +43,61 @@ class Fire(nn.Module):
         ], dim=1)
 
 class SqueezeNet(nn.Module):
-    def __init__(self, version='1_0', num_classes=8):
+    def __init__(self, num_classes=8, num_dense_layer=3, num_input=3):
         super().__init__()
         self.num_classes = num_classes
-        if version == '1_0':
+        self.num_input = num_input
+        if self.num_input == 3:
+            modules= []
+            _in_channels = 1
+            for i in range(num_dense_layer):
+                modules.append(MakeDense(_in_channels, growth_rate=32))
+                _in_channels += growth_rate
+            self.head_spt = nn.Sequential(*modules)
+            self.head_chr = nn.Sequential(*modules)
+            self.head_mfc = nn.Sequential(*modules)
+            self.features = nn.Sequential(
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                Fire(99, 16, 64, 64),
+                Fire(128, 16, 64, 64),
+                Fire(128, 32, 128, 128),
+                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                Fire(256, 32, 128, 128),
+                Fire(256, 48, 192, 192),
+                Fire(384, 48, 192, 192),
+                Fire(384, 64, 256, 256),
+                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                Fire(512, 64, 256, 256),
+            )
+        elif self.num_input == 7:
+            modules= []
+            _in_channels = 1
+            for i in range(num_dense_layer):
+                modules.append(MakeDense(_in_channels, growth_rate=12))
+                _in_channels += growth_rate
+            self.head_spt = nn.Sequential(*modules)
+            self.head_chr = nn.Sequential(*modules)
+            self.head_cqt = nn.Sequential(*modules)
+            self.head_cens = nn.Sequential(*modules)
+            self.head_mfc = nn.Sequential(*modules)
+            self.head_mfc1d = nn.Sequential(*modules)
+            self.head_mfc2d = nn.Sequential(*modules)
+            self.features = nn.Sequential(
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                Fire(91, 16, 64, 64),
+                Fire(128, 16, 64, 64),
+                Fire(128, 32, 128, 128),
+                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                Fire(256, 32, 128, 128),
+                Fire(256, 48, 192, 192),
+                Fire(384, 48, 192, 192),
+                Fire(384, 64, 256, 256),
+                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                Fire(512, 64, 256, 256),
+            )
+        elif self.num_input == 1:
             self.features = nn.Sequential(
                 nn.Conv2d(1, 96, kernel_size=7, stride=2),
                 nn.ReLU(inplace=True),
@@ -50,25 +113,9 @@ class SqueezeNet(nn.Module):
                 nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
                 Fire(512, 64, 256, 256),
             )
-        elif version == '1.1':
-            self.features = nn.Sequential(
-                nn.Conv2d(1, 64, kernel_size=3, stride=2),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
-                Fire(64, 16, 64, 64),
-                Fire(128, 16, 64, 64),
-                nn.MaxPool2d(kernel_size=3, stride=2, ceol_mode=True),
-                Fire(128, 32, 128, 128),
-                Fire(256, 32, 128, 128),
-                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
-                Fire(256, 48, 192, 192),
-                Fire(384, 48, 192, 192),
-                Fire(384, 48, 256, 256),
-                Fire(512, 64, 256, 256),
-            )
         else:
-            raise ValueError("Unsupport SqueezeNet version {version}:"
-                             "1_0 or 1_1 expected".format(version=version))
+            raise ValueError("Unsupport SqueezeNet input type {input}:"
+                             "1_0 or 1_1 expected".format(input=num_input))
         final_conv=nn.Conv2d(512, self.num_classes, kernel_size=1)
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5),
@@ -86,6 +133,20 @@ class SqueezeNet(nn.Module):
                     nn.init.constant_(m.bias, 0)
         
     def forward(self, x):
+        if self.num_input == 3:
+            x[0] = self.head_spt(x[0])
+            x[1] = self.head_chr(x[1])
+            x[2] = self.head_mfc(x[2])
+            x = torch.cat((x[0], x[1], x[2]), 1)
+        elif self.num_input == 7:
+            x[0] = self.head_spt(x[0])
+            x[1] = self.head_chr(x[1])
+            x[2] = self.head_cqt(x[1])
+            x[3] = self.head_cens(x[1])
+            x[4] = self.head_mfc(x[4])
+            x[5] = self.head_mfc(x[5])
+            x[6] = self.head_mfc(x[6])
+            x = torch.cat((x[0], x[1], x[2], x[3], x[4], x[5], x[6]), 1)
         x = self.features(x)
         x = self.classifier(x)
         return torch.flatten(x, 1)
